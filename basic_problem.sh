cfbfbae3104fa6fcf8ad6279044dfa5bc165d9dc
Kumarivarsha-14 <varshakumari2714@gmail.com>
2023-03-01 19:27:36 -0800

    [varsha] Add:Multiply to number using user input

--- basic_problem.sh
+++ basic_problem.sh
@@ -1,20 +1,20 @@
-                                                                                             Answer -1 ( subtraction program)
-# Write a program  subtraction of two numbers
+                                                                                                     #Answer -1 ( Multiplication program)                                                                
+
+ # Write a program  multiplication of two numbers
+
+ # schebang constructor
+#!/bin/bash -x
 
 
-# schebang constructor
-#!/bin/bash -x   //for file execution
-
- # read the first number by user input
+# read the first number by user input
 read -p "Enter the First Number: " num1
 
-# read the  second number by user input
+ # read the  second number by user input
 read -p "Enter the Second Number: " num2
 
-
-# subtraction to the value of num1 and num2
-  total_subtract=$(($num1 - $num2))
+# multiply to the value of num1 and num2
+  total_multi=$(($num1  * $num2))
 
   
-   # print the total value 
-  echo "The total value is:" $total_subtract
+   # print the total  value 
+  echo "The total value is:" $total_multi
