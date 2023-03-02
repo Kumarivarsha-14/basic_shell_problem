b630f2d12c2c9cb751f9b2e14d50f747cebb489b
Kumarivarsha-14 <varshakumari2714@gmail.com>
2023-03-01 19:25:10 -0800

    [varsha] Add:Subtract two number using user input

--- basic_problem.sh
+++ basic_problem.sh
@@ -1,19 +1,20 @@
-                                          # Answer -1 (Adding program)
+                                                                                             Answer -1 ( subtraction program)
+# Write a program  subtraction of two numbers
 
- #   Write a Program adding of number
-      
- # Shebang Constructor
- #!/bin/bash -x         //for file execution command
 
-# read the first number by user input
- read -p  "Enter the first number : " num1
- 
- 
+# schebang constructor
+#!/bin/bash -x   //for file execution
+
+ # read the first number by user input
+read -p "Enter the First Number: " num1
+
 # read the  second number by user input
- read -p  "Enter the  second number :" num2
+read -p "Enter the Second Number: " num2
 
-# add the number of num1 and num2
-total_addition=$(($num1+$num2))
 
-# print the total value
-echo " The total sum of the value are : " $total_addition
+# subtraction to the value of num1 and num2
+  total_subtract=$(($num1 - $num2))
+
+  
+   # print the total value 
+  echo "The total value is:" $total_subtract
