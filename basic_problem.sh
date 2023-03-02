d45496beb656e0e0ba4f5c8e409aeb95842880d8
Kumarivarsha-14 <varshakumari2714@gmail.com>
2023-03-01 19:30:00 -0800

    [varsha] Add:Division to number using user input

--- basic_problem.sh
+++ basic_problem.sh
@@ -1,20 +1,21 @@
-                                                                                                     #Answer -1 ( Multiplication program)                                                                
 
- # Write a program  multiplication of two numbers
+                              #Answer -1 (Division program)
 
- # schebang constructor
-#!/bin/bash -x
+# Write a program  division of two numbers
+
+# schebang constructor
+ #!/bin/bash -x     //for file execution
 
 
 # read the first number by user input
 read -p "Enter the First Number: " num1
 
- # read the  second number by user input
+
+# read the  second number by user input
 read -p "Enter the Second Number: " num2
 
-# multiply to the value of num1 and num2
-  total_multi=$(($num1  * $num2))
+ # division to the value of num1 and num2
+  total_divi=$(($num1  / $num2))
 
-  
-   # print the total  value 
-  echo "The total value is:" $total_multi
+   # print the total value  
+  echo " The total value of is:" $total_divi
